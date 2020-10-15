8f4d9ce21a53ef2bf28dd851d7ed1fdd42652eff
fatimaSmoussawi <fatimasamoussawi@gmail.com>
2020-10-15 17:39:19 +0300

    Weather app with CSS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+
 import Titles from "./components/Titles";
-import Forms from "./components/Form";
+import Form from "./components/Form";
 import Weather from "./components/Weather";
- const API_KEY="92dc613982178411700af025db60a69c";
-class App extends React.Component{
+
+const API_KEY = "3585775f387b0d0cba6c5b3dc41b8167";
+
+class App extends React.Component {
   state = {
     temperature: undefined,
     city: undefined,
@@ -13,53 +16,59 @@ class App extends React.Component{
     error: undefined
   }
   getWeather = async (e) => {
+    e.preventDefault();
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
- 
-    e.preventDefault();
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`)
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
     const data = await api_call.json();
-
-    if (city && country){
-    this.setState ({
-      temperature: data.main.temp,
-      city: data.name,
-      country: data.sys.country,
-      humidity: data.main.humidity,
-      description: data.weather[0].description,
-      error: ""
-    });
-  }
-    else {
+    if (city && country) {
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""
+      });
+    } else {
       this.setState({
         temperature: undefined,
         city: undefined,
-        country: undefined, 
+        country: undefined,
         humidity: undefined,
         description: undefined,
         error: "Please enter the values."
       });
-    
+    }
   }
-  }
-  render(){
-  return(
-    <div> 
-      <Titles/>
-      <Forms getWeather = {this.getWeather}/>
-      <Weather
+  render() {
+    return (
+      <div>
+        <div className="wrapper">
+          <div className="main">
+            <div className="container">
+              <div className="row">
+                <div className="col-xs-5 title-container">
+                  <Titles />
+                </div>
+                <div className="col-xs-7 form-container">
+                  <Form getWeather={this.getWeather} />
+                  <Weather 
                     temperature={this.state.temperature} 
                     humidity={this.state.humidity}
                     city={this.state.city}
                     country={this.state.country}
                     description={this.state.description}
                     error={this.state.error}
-      
-      
-      />
-    </div>
-  );
-  
-}
-}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+};
+
 export default App;
